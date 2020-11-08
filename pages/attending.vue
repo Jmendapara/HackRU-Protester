c41b75976c1fac5c2c10ae978a266e6204e15080
Jay Mendapara <jmendapara@gmail.com>
2020-11-08 01:11:28 -0500

    added attnding page

--- pages/attending.vue
+++ pages/attending.vue
@@ -1,36 +1,125 @@
 <template>
+  <v-container>
+    <div class="create">
+      <h1>Attending Protests</h1>
+    </div>
 
-<container>
-<div id="array">
-    <v-row no-gutters>
-    <GridItem v-for="protest in protests" :key="protest.title" :protest="protest"></GridItem>
-    </v-row>
-</div>
-</container>
+    <div class="organizedProtestList">
+      <v-row>
+        <v-col
+          cols="12"
+          sm="3"
+          md="3"
+          v-for="protest in myProtests"
+          :key="protest.title"
+        >
+          <GridItem :protest="protest" @card-clicked="handleCardClick($event)"></GridItem>
+        </v-col>
+      </v-row>
+    </div>
 
+    <v-dialog v-model="dialog" width="80%">
+      <template>
+        
+      </template>
+
+      <Protest :protestInfo="selectedProtest"></Protest>
+    </v-dialog>
+  </v-container>
 </template>
 
+<style scoped>
+/* .create{
+    position: absolute;
+    top:70px;
+    right:40px;
+}
+div h1{
+    position: absolute;
+    top:70px;
+    left: 60px;
+    letter-spacing: 0.08em;
+    font-family: Spartan, Sans-Serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 40px;
+    line-height: 45px;
+} */
 
+.create {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  letter-spacing: 0.06em;
+  font-family: Spartan, Sans-Serif;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 45px;
+}
+
+.organizedProtestList {
+}
+</style>
 
 <script>
-
-import GridItem from './../components/GridItem.vue'
-import Component from 'vue-class-component'
-import Vue from 'vue'
-
-
+import { fireDb } from "~/plugins/firebase.js";
+import Vue from "vue";
+import Component from "nuxt-class-component";
+import { userStore } from "~/store/userStore";
+import * as firebase from "firebase/app";
+import GridItem from "./../components/GridItem.vue";
 
 @Component({})
 export default class Attending extends Vue {
 
-    protests= [{title: 'Ashley'},{title: 'Andy'},{title: 'Andrew'},{title: 'Chad'},{title: 'Hiep'},{title: 'Sarah'},{title: 'David'}];
+  protests = [];
+  dialog=false;
+  selectedProtest={};
+  attendingProtests = []; 
 
+  async mounted() {
+    //console.log(userStore.userUsername);
+    //ar protests = userStore.userOrganizedProtests;
+    //console.log(userStore.userOrganizedProtests);
+
+    const protests = await fireDb.collection("protests");
+    protests.get().then((querySnapshot) => {
+      let tempProtests = [];
+      querySnapshot.docs.forEach((doc) => {
+        tempProtests.push(doc.data());
+      });
+      this.protests = tempProtests;
+    });
+
+    const attendingProtests = await fireDb.collection("users")
+        .doc(userStore.userUsername)
+        .get().then(
+            (userInfo) => this.attendingProtests = userInfo.data().attendingProtests
+        );
+
+  }
+
+  handleCardClick(event){
+    
+    this.selectedProtest = this.myProtests.find(protest => protest.protestID == event);
+    this.dialog = true;
+
+  }
+
+
+  get myProtests() {
+
+    let tempProtests = [];
+
+    for(let i = 0; i < this.attendingProtests.length; i++){
+        const currId = this.attendingProtests[i];
+        tempProtests.push(this.protests.find(protest => protest.protestID == currId));
+    }
+
+    return tempProtests;
+
+  }
 }
 </script>
-
-<style>
-#gridelement {
-    color: #ffffff
-}
-
-</style>
